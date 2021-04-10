d5cb89c589a3d6a961f6d8faa5e71086c27ee83a
nateharker <70962388+nateharker@users.noreply.github.com>
2021-04-10 02:41:13 -0600

    so many changes
    added lots of features to profile mgmt, registration info update, and role management

---
+++ Models/AppUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FagElGamous.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserType { get; set; }
+        public string OrganizationName { get; set; }
+        public int UsernameChangeLimit { get; set; } = 10;
+        public byte[] ProfilePicture { get; set; }
+  
+    }
+}
