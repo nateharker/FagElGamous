56ce0b6a04ea26837e7b377f22a4ddf7638468c3
nateharker <70962388+nateharker@users.noreply.github.com>
2021-04-14 00:20:37 -0600

    fixed formatting and making search form

---
+++ Models/LocSearch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FagElGamous.Models
+{
+    public class LocSearch
+    {
+        [Required]
+        public string NorthSouth { get; set; }
+        [Required]
+        public int NSLow { get; set; }
+        public int NSHigh { get; set; }
+        [Required]
+        public string EastWest { get; set; }
+        [Required]
+        public int EWLow { get; set; }
+        public int EWHigh { get; set; }
+        public string Subplot { get; set; }
+        public int BurialNumber { get; set; }
+    }
+}
